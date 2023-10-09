93edbc955dde57474f3ec7ed8faf2f2849ca23cc
Bibek Regmi <bibek.regmi@swifttech.com>
2023-10-09 15:06:07 +0545

    grpc transcoder learning

--- grpcCrud/server.js
+++ grpcCrud/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv')
 const grpc = require('@grpc/grpc-js')
 const protoLoader = require('@grpc/proto-loader')
-
+const proto = require('google-proto-files');
 
 const envPath = `${__dirname}/.env`
 dotenv.config({ path: envPath })
@@ -13,29 +13,58 @@ const port = process.env.PORT;
 const dirname = `${__dirname}`
 const testpath = `./../common/proto/simpleCrud.rpc.proto`
 
+const tempPath =`./../common/tempProto/simpleCrud.proto`
 const protoPath = `${dirname}/${testpath}`
 
+const tempProtoPath = `${dirname}/${tempPath}`
 
-const packageDefinition = protoLoader.loadSync(protoPath, {
+const packageDefinition = protoLoader.loadSync(tempProtoPath, {
   keepCase: true,
   longs: 'string',
   defaults: true,
 })
 
+
+
+
 const server = new grpc.Server()
 
 
-const simpleProto = grpc.loadPackageDefinition(packageDefinition)
+// const simpleProto = grpc.loadPackageDefinition(packageDefinition)
+
+// const simpleServiceA = require('./controllers/index');
+
+// server.addService(simpleProto.example.simpleCrud.rpc.simpleCrudService.service, {
+//   create: simpleServiceA.createStudents,
+//   read: simpleServiceA.readStudents,
+//   readAll: simpleServiceA.readAllStudents,
+//   update: simpleServiceA.updateStudents,
+//   delete: simpleServiceA.deleteStudents
+// })
+// console.log(simpleProto);
+
+const tempServer = async()=>{
+  
+
+
+  // const files = proto.getProtoPath('logging','v2');
+
+  const loadPackage = await proto.load('./proto/simpleCrud.rpc.proto');
 
 const simpleServiceA = require('./controllers/index');
 
-server.addService(simpleProto.example.simpleCrud.rpc.simpleCrudService.service, {
+server.addService(simpleServiceA.example.simpleCrud.rpc.simpleCrudService.service, {
   create: simpleServiceA.createStudents,
   read: simpleServiceA.readStudents,
   readAll: simpleServiceA.readAllStudents,
   update: simpleServiceA.updateStudents,
   delete: simpleServiceA.deleteStudents
 })
+
+
+}
+
+tempServer();
 
 server.bindAsync(
   `${process.env.GRPC_HOST}:${port}`,
